3fe2551152de71ed71515e9e0a98695f9fb303cc
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-04-02 17:00:52 +0900

    add password in user

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'dotenv-rails'
 gem 'pry-rails'
 gem 'pry-byebug'
 
+gem 'bcrypt'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :tel, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum:5 }
+
+  has_secure_password
 end
